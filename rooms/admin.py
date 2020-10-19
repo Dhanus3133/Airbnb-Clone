5066661229b62474c1e54f58647ae214f439fdce
Dhanus SL <dhanus3133@gmail.com>
2020-10-19 22:27:51 +0530

    Completed seeds...

--- rooms/admin.py
+++ rooms/admin.py
@@ -96,6 +96,7 @@ class RoomAdmin(admin.ModelAdmin):
     def count_photos(self, obj):
         return obj.photos.count()
 
+    count_photos.short_description = "Photos Count"
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
