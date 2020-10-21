699f273548145d6ed1c90d1bbc2a531705e1bc46
Dhanus SL <dhanus3133@gmail.com>
2020-10-21 13:57:09 +0530

    Completed the HomeView

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,13 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, per_page=10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
-    return render(request, 'rooms/home.html', context={
-        "rooms": rooms
-    })
+class HomeView(ListView):
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    context_object_name = 'rooms'
+    ordering = 'created'
