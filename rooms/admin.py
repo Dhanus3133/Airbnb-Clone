7fdb1b54d254c14c1b0d3e55acc8bba34abea5d2
Dhanus SL <dhanus3133@gmail.com>
2020-10-21 22:57:00 +0530

    Completed the Filtering in DetailView

--- rooms/admin.py
+++ rooms/admin.py
@@ -29,7 +29,7 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description","city", "country", "address", "price",)}
+            {"fields": ("name", "description","city", "country", "address", "price", "room_type",)}
         ),
         (
             "Times",
