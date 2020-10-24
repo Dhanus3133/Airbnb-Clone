ef9d0d7d6bdc378b50d5ada13c12cc85dfae4339
Dhanus SL <dhanus3133@gmail.com>
2020-10-24 18:11:24 +0530

    Finished USER SIGN UP

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,6 @@ from . import models
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
-    print(email, password)
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -18,3 +17,29 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Wrong Password"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User Does not exist!"))
+
+
+class SignUpForm(forms.ModelForm):
+
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email",)
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("Password Confirmation Doesn't Match!")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
